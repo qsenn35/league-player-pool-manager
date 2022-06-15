507c5a5c4deeaa703f5a60e692b5369ce1eac58a
helloitsian <15301476+helloitsian@users.noreply.github.com>
2022-06-15 01:12:59 -0500

    typo fix

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-const SERVER_URL = proccess.env.NODE_ENV === "production" ? "http://66.228.51.122/api" : "http://localhost:3000/api"
+const SERVER_URL = process.env.NODE_ENV === "production" ? "http://66.228.51.122/api" : "http://localhost:3000/api"
 
 export {
   SERVER_URL,
